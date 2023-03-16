afe72800f08ac1f8c3e235961bee1b3734ddcef1
Dmytro <109168390+TaraniukD@users.noreply.github.com>
2023-03-16 14:01:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import {Provider} from 'react-redux';
-import store, { persistor } from 'redux/store';
-import { PersistGate } from 'redux-persist/integration/react'
+import store from 'redux/store';
+
 import './index.css';
 
 
